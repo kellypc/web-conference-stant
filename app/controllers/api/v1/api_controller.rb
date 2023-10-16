b6571900cccb883908a7ba938a8cc46c428f4369
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2023-10-16 14:28:46 -0300

    Add support file

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ApiController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :object_not_found
+
+    def object_not_found
+      render json: 'Object not found', status: :not_found
+    end
+end
