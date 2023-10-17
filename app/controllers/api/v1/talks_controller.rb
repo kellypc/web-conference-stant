51bc75de9647e0955f77a0c0bde845402f185096
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2023-10-16 21:32:18 -0300

    Refactoring controller

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -7,15 +7,21 @@ class Api::V1::TalksController < Api::V1::ApiController
   end
   
   def create
+    read_file_and_save_talk
+
+    render json: { message: "Arquivo recebido com sucesso"}
+  end
+
+  def destroy
+  end
+
+  private
+
+  def read_file_and_save_talk
     CSV.foreach(params[:talks], col_sep: ",") do |row|
       talk = Talk.find_or_initialize_by(title: row[0].strip)
       talk.duration = row[1]&.strip&.gsub("min", "") || "5"
       talk.save!
-    end 
-
-    render json: { message: "Arquivo recebido com sucesso" }, status: :ok
-  end
-
-  def destroy
+    end
   end
 end
